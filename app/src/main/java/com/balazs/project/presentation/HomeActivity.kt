3d6fa526eaa43de2578f0cdd79b317d9b698d4f5
negrea_balazs <62569008+nbalazs1337@users.noreply.github.com>
2023-04-26 16:31:52 +0300

    did work from work

--- app/src/main/java/com/balazs/project/presentation/HomeActivity.kt
+++ app/src/main/java/com/balazs/project/presentation/HomeActivity.kt
@@ -1,36 +1,37 @@
 package com.balazs.project.presentation
 
-import Adapter
+import android.R
 import android.content.Intent
-import android.content.res.ColorStateList
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.navigation.Navigation
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.balazs.project.R
-import com.balazs.project.data.model.Data
 import com.balazs.project.utils.TransparentStatusBarHandler
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import de.hdodenhof.circleimageview.CircleImageView
+
 
 class HomeActivity : AppCompatActivity(){
 
 
     private lateinit var auth : FirebaseAuth
-
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
     private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(com.balazs.project.R.layout.activity_home)
         TransparentStatusBarHandler.initTransparentStatusBar(window)
         auth = FirebaseAuth.getInstance()
 
@@ -38,9 +39,9 @@ class HomeActivity : AppCompatActivity(){
 
         val email = intent.getStringExtra("email")
         val displayName = intent.getStringExtra("name")
-        findViewById<TextView>(R.id.txt_name).text = "Hi, ${displayName}"
+        findViewById<TextView>(com.balazs.project.R.id.txt_name).text = "Hi, ${displayName}"
 
-        findViewById<Button>(R.id.button_signout).setOnClickListener {
+        findViewById<CircleImageView>(com.balazs.project.R.id.button_signout).setOnClickListener {
             auth.signOut()
             val googleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN)
             googleSignInClient.signOut().addOnCompleteListener(this) {
@@ -49,29 +50,71 @@ class HomeActivity : AppCompatActivity(){
             }
         }
 
+        val drawerLayout = findViewById<DrawerLayout>(com.balazs.project.R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(com.balazs.project.R.id.navigation_view)
+        // Call findViewById on the DrawerLayout
+
+
+        // Pass the ActionBarToggle action into the drawerListener
+        //actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+       // drawerLayout.addDrawerListener(actionBarToggle)
+
+        // Display the hamburger icon to launch the drawer
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Call syncState() on the action bar so it'll automatically change to the back button when the drawer layout is open
+//        actionBarToggle.syncState()
+
+
+        // Call findViewById on the NavigationView
+
+        // Call setNavigationItemSelectedListener on the NavigationView to detect when items are clicked
+        TransparentStatusBarHandler.initTransparentStatusBar(window)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.message -> {
+                    Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.home -> {
+                    Toast.makeText(this, "People", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.message -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+            supportFragmentManager.findFragmentById(com.balazs.project.R.id.fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView = findViewById(com.balazs.project.R.id.bottom_navigation_view)
+        bottomNavigationView.itemIconTintList = null
         bottomNavigationView.setupWithNavController(navController)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_tenant -> {
-                    navController.navigate(R.id.homeFragment)
+                com.balazs.project.R.id.navigation_tenant -> {
+                    navController.navigate(com.balazs.project.R.id.homeFragment)
                     //item.setIcon(R.drawable.ic_home_green)
                     item.setCheckable(true)
                     // switch to Home fragment or activity
                     true
                 }
-                R.id.navigation_landlord -> {
-                    navController.navigate(R.id.chatFragment)
+                com.balazs.project.R.id.navigation_landlord -> {
+                    navController.navigate(com.balazs.project.R.id.chatFragment)
                     //item.setIcon(R.drawable.ic_message)
                     item.setCheckable(true)
                     // switch to Dashboard fragment or activity
                     true
                 }
-                R.id.navigation_worker -> {
-                    navController.navigate(R.id.settingsFragment)
+                com.balazs.project.R.id.navigation_worker -> {
+                    navController.navigate(com.balazs.project.R.id.settingsFragment)
                     //item.setIcon(R.drawable.ic_settings_green)
                     item.setCheckable(true)
                     // switch to Notifications fragment or activity
