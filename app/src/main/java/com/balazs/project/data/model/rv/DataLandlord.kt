610aba93e34c30c5fa8ee49340abb8255ac91d8d
negrea_balazs <62569008+nbalazs1337@users.noreply.github.com>
2023-06-20 13:04:21 +0300

    review system setup done

--- app/src/main/java/com/balazs/project/data/model/rv/DataLandlord.kt
+++ app/src/main/java/com/balazs/project/data/model/rv/DataLandlord.kt
@@ -4,6 +4,7 @@ data class DataLandlord(
     val iv_coverID: Int,
     val txt_title: String,
     val txt_name: String,
+    var txt_average: String,
     val txt_price: String,
     val iv_rightID: Int,
 )
