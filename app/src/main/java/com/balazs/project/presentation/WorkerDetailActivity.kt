735b4f5539426602675aa1dcf8bcd6695847178b
negrea_balazs <62569008+nbalazs1337@users.noreply.github.com>
2023-07-08 19:50:07 +0300

    donex2

--- app/src/main/java/com/balazs/project/presentation/WorkerDetailActivity.kt
+++ app/src/main/java/com/balazs/project/presentation/WorkerDetailActivity.kt
@@ -1,8 +1,10 @@
 package com.balazs.project.presentation
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import com.balazs.project.R
 import com.balazs.project.utils.TransparentStatusBarHandler
 
@@ -15,5 +17,23 @@ class WorkerDetailActivity : AppCompatActivity() {
         btn_back.setOnClickListener {
             finish()
         }
+        val sharedPreferences = getSharedPreferences("MyPrefsWorker", Context.MODE_PRIVATE)
+        val service = sharedPreferences.getString("service", "") ?: ""
+        val price = sharedPreferences.getString("price", "") ?: ""
+        val city = sharedPreferences.getString("city", "") ?: ""
+        val description = sharedPreferences.getString("description", "") ?: ""
+
+        val txt_title = findViewById<TextView>(R.id.txt_title_worker_job)
+        val txt_price = findViewById<TextView>(R.id.txt_price_worker_per_hour)
+        val txt_city = findViewById<TextView>(R.id.textView)
+        val txt_description = findViewById<TextView>(R.id.textView4)
+
+        txt_title.text = service.toString()
+        txt_price.text = price.toString()
+        txt_city.text = city.toString()
+        txt_description.text = description.toString()
+
+
+
     }
 }
